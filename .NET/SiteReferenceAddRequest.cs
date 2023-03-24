a3eac194207f782aca2730a864131c04f8238c98
kelvin lao <48492707+kelvin-lao-01@users.noreply.github.com>
2023-03-23 23:05:14 -0700

    Create SiteReferenceAddRequest.cs

---
+++ .NET/SiteReferenceAddRequest.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sabio.Models.Requests.SiteReferences
+{
+    public class SiteReferenceAddRequest
+    {
+        public int SiteReferenceId { get; set; }   
+     
+    }
+}
